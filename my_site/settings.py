2ea4dc29c77aa3fb4beaf695fd7e631cabb05682
ali <a.saleh.m.a@gmail.com>
2020-06-03 23:22:19 +0430

    change database to postgresql

--- my_site/settings.py
+++ my_site/settings.py
@@ -77,9 +77,14 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mysite',
+        'USER': 'admin',
+        'PASSWORD': 'alleportedelsole',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
+
 }
 
 
