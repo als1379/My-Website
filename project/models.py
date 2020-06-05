9a2070b29e60936514e4336bd0ca2031c39e3261
ali <a.saleh.m.a@gmail.com>
2020-06-05 23:56:39 +0430

    fix ui and model

--- project/models.py
+++ project/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=400)
+    sub_title = models.CharField(max_length=250)
+    description = models.TextField()
     language = models.CharField(max_length=50)
-
     link = models.URLField()
-    image = models.ImageField(upload_to='profile/projects', blank=True)
+    image = models.ImageField(upload_to='project/', blank=True)
